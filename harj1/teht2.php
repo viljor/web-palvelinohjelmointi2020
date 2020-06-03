030ec4239e20ce3ca0afb765fda199bff9bc57cd
Ville Jormanainen <jormanainen.ville@gmail.com>
2020-06-03 17:34:40 +0300

    poistettu vanha versio teht 2

--- harj1/teht2.php
+++ harj1/teht2.php
@@ -1,11 +1,29 @@
-<title>Teht 2: Eurolaskin</title>
-<form method="get" action="<?php echo $_SERVER['PHP_SELF']?>">
-        Muunnettava euromäärä: <br><input type="number" name="eurot"><br>
-        <input type="submit" name="painike" value="Lähetä">
-</form>
-
 <?php
-    if (!isset($_GET['painike'])) exit();
-    $markat = $_GET['eurot'] * 5.94573;
-    echo "<strong>{$markat}</strong> markkaa<br>";
+    $eurot = isset($_GET['eurot']) ? $_GET['eurot'] : 0;
+    $markka_kerroin = 5.94573;
+    $markat = $eurot * $markka_kerroin;
+
+    $head = <<<EOHead
+        <title>Eurolaskin.php</title>
+    EOHead;
+
+    $lomake = <<< EOLomake
+        <form method="get" action="{$_SERVER['PHP_SELF']}">
+            Eurot:<br>
+            <input type="text" name="eurot" value="$eurot"><br>
+            <br><br>
+            <input type="submit" name="painike" value="Lähetä">
+        </form>
+    EOLomake;
+
+    $footer = <<<EOFooter
+        <hr>
+        <p>{$markat} markkaa</p>
+        <i>Muunnoskertoimen lähde: <a href="http://www.stat.fi/tup/laskurit/rahanarvonmuunnin.html">http://www.stat.fi/tup/laskurit/rahanarvonmuunnin.html</a></i>
+    EOFooter;
+
+    echo $head;
+    echo $lomake;
+    echo $footer;
+
 ?>
